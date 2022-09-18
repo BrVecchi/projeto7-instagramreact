867147875f6890d921fe80bbde9b771b11f517b2
BrVecchi <bno.vecchi@gmail.com>
2022-09-18 16:11:59 -0300

    add funcionalidade de alterar imagem de usuario

--- src/Usuario.js
+++ src/Usuario.js
@@ -2,46 +2,50 @@ import React from "react";
 import { Pencil } from "react-ionicons";
 import usuario from "./imgs/catanacomics 1.png";
 
-
-
 function UsuarioInfo(props) {
   const [nome, setNome] = React.useState("Catana");
+  const [img, setImg] = React.useState(usuario);
 
   return (
     <div class="info-pessoal">
-      <img src={props.img} />
+      <img
+        class="usuario-principal-img hover"
+        src={img}
+        onClick={() => {
+          const novaImg = prompt("Digite a URL da nova imagem: ");
+          if (novaImg !== null && novaImg !== "") {
+            setImg(novaImg);
+          }
+        }}
+      />
       <div class="usuario-principal-info">
         <p class="usuario-principal-texto-user">{props.textUser}</p>
         <div class="nome-lapis">
-        <p class="usuario-principal-texto-name">{nome}</p>
-        <Pencil height={"14px"} width={"14px"} onClick={()=> {
-          const novoNome = prompt("Qual nome quer colocar?")
-          console.log(novoNome)
-          if (novoNome !== null && novoNome !==""){
-            setNome(novoNome)
-          }
-        }}/>
+          <p class="usuario-principal-texto-name">{nome}</p>
+          <Pencil
+            cssClasses={"hover"}
+            height={"14px"}
+            width={"14px"}
+            onClick={() => {
+              const novoNome = prompt("Qual nome quer colocar?");
+              if (novoNome !== null && novoNome !== "") {
+                setNome(novoNome);
+              }
+            }}
+          />
         </div>
       </div>
     </div>
   );
 }
 
-
-
 export default function Usuario() {
-
-  
-  const usuariosInfo = [
-    {img: usuario, textUser: "catanacomics"},
-  ];
-
-  
+  const usuariosInfo = [{ img: usuario, textUser: "catanacomics" }];
 
   return (
     <>
       {usuariosInfo.map((u) => (
-        <UsuarioInfo img={u.img} textUser={u.textUser}/>
+        <UsuarioInfo img={u.img} textUser={u.textUser} />
       ))}
     </>
   );
